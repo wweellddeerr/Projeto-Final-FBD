eb81403fa035da8cd6c32ad472a9542368a156a5
Welder Luz <wweellddeerr@gmail.com>
2016-12-04 14:30:36 -0200

    Ajustando o povoamento referente ao código de municípios.

--- ETL/src/br/unb/ppca/fbd/etl/action/PovoadorGenerico.java
+++ ETL/src/br/unb/ppca/fbd/etl/action/PovoadorGenerico.java
@@ -33,7 +33,7 @@ public abstract class PovoadorGenerico {
 	public static final int NUMERO_ATRIBUTOS = 44;
 
 	protected File file;
-	
+
 	private EntityManager entityManager;
 
 	public abstract void povoar() throws DiretorioInvalidoException, ArquivoInvalidoException;
@@ -63,13 +63,14 @@ public abstract class PovoadorGenerico {
 
 	protected void povoarDados(UF uf, ArquivoProcessado arquivoProcessado) {
 		for (LinhaProcessada linhaProcessada : arquivoProcessado.getLinhasProcessadas()) {
+			System.out.println(linhaProcessada);
+
 			// cria os municípios de eleição
-			criarMunicipioSeNaoExistir(linhaProcessada.getCodigoMunicipio(), linhaProcessada.getNomeMunicipio(), uf);
-		}
-		for (LinhaProcessada linhaProcessada : arquivoProcessado.getLinhasProcessadas()) {
+			Municipio municipioEleicao = criarMunicipioSeNaoExistir(linhaProcessada.getNomeMunicipio(), uf);
+
 			// cria os partidos políticos
-			criarPartidoSeNaoExistir(linhaProcessada.getNumeroPartido(), linhaProcessada.getSiglaPartido(),
-					linhaProcessada.getNomePartido());
+			PartidoPolitico partido = criarPartidoSeNaoExistir(linhaProcessada.getNumeroPartido(),
+					linhaProcessada.getSiglaPartido(), linhaProcessada.getNomePartido());
 
 			// cria as nacionalidade
 			criarNacionalidadeSeNaoExistir(linhaProcessada.getCodigoNacionalidade(),
@@ -81,38 +82,38 @@ public abstract class PovoadorGenerico {
 			// cria os graus de instrução
 			criarGrauInstrucaoSeNaoExistir(linhaProcessada.getCodigoGrauInstrucao(),
 					linhaProcessada.getDescricaoGrauInstrucao());
-			
-			//cria os municípios de nascimento
-			System.out.println(linhaProcessada);
-			linhaProcessada.setCodigoMunicipioNascimento(criarMunicipioSeNaoExistir(linhaProcessada.getCodigoMunicipioNascimento(), 
-					linhaProcessada.getNomeMunicipioNascimento(), UF.valueOf(linhaProcessada.getSiglaUFNascimento())).getCodigoTSE());
+
+			// cria os municípios de nascimento
+			Municipio municipioNascimento = criarMunicipioSeNaoExistir(linhaProcessada.getNomeMunicipioNascimento(),
+					UF.valueOf(linhaProcessada.getSiglaUFNascimento()));
 
 			// cria os candidatos
-			criarCandidatoSeNaoExistir(linhaProcessada.getCpf(), linhaProcessada.getCodigoMunicipioNascimento(),
-					linhaProcessada.getCodigoNacionalidade(), linhaProcessada.getCodigoOcupacao(),
-					linhaProcessada.getCodigoGrauInstrucao(), linhaProcessada.getNomeCandidato(),
-					linhaProcessada.getDataNascimento(), linhaProcessada.getNumeroTitulo(), linhaProcessada.getEmail(),
-					linhaProcessada.getSexo(), linhaProcessada.getEstadoCivil());
+			Candidato candidato = criarCandidatoSeNaoExistir(linhaProcessada.getCpf(),
+					municipioNascimento.getCodigoTSE(), linhaProcessada.getCodigoNacionalidade(),
+					linhaProcessada.getCodigoOcupacao(), linhaProcessada.getCodigoGrauInstrucao(),
+					linhaProcessada.getNomeCandidato(), linhaProcessada.getDataNascimento(),
+					linhaProcessada.getNumeroTitulo(), linhaProcessada.getEmail(), linhaProcessada.getSexo(),
+					linhaProcessada.getEstadoCivil());
 
 			// cria as candidaturas
-			criarCandidaturaSeNaoExistir(linhaProcessada);
+			criarCandidaturaSeNaoExistir(linhaProcessada.getAnoCandidatura(), candidato.getCpf(), partido.getNumero(),
+					municipioEleicao.getCodigoTSE(), linhaProcessada.getNumeroUrna(), linhaProcessada.getNomeUrna(),
+					linhaProcessada.getDescricaoSituacaoCandidatura(), linhaProcessada.getNomeCargo(),
+					linhaProcessada.getSituacaoEleicao(), linhaProcessada.getNumeroTurno());
 		}
 
 	}
 
-	protected void criarCandidaturaSeNaoExistir(LinhaProcessada linhaProcessada) {
-		
-		Candidatura c = getEntityManager().find(Candidatura.class, new CandidaturaId(linhaProcessada.getAnoCandidatura(), linhaProcessada.getCpf()));
+	protected void criarCandidaturaSeNaoExistir(Integer anoCandidatura, Long cpf, Integer numeroPartido,
+			Integer codigoMunicipioCandidatura, Integer numeroUrna, String nomeUrna, String situacaoCandidatura,
+			String nomeCargo, String situacaoEleicao, Integer numeroTurno) {
 
-		if(c == null) {
-			getEntityManager().persist(new Candidatura(linhaProcessada.getAnoCandidatura(), linhaProcessada.getCpf(),
-					linhaProcessada.getNumeroPartido(), linhaProcessada.getCodigoMunicipio(),
-					linhaProcessada.getNumeroUrna(), linhaProcessada.getNomeUrna(),
-					linhaProcessada.getDescricaoSituacaoCandidatura(),
-					linhaProcessada.getNomeCargo(),
-					linhaProcessada.getSituacaoEleicao(),
-					determinarSeSegundoTurno(linhaProcessada.getNumeroTurno()))
-				);
+		Candidatura c = getEntityManager().find(Candidatura.class, new CandidaturaId(anoCandidatura, cpf));
+
+		if (c == null) {
+			getEntityManager().persist(new Candidatura(anoCandidatura, cpf, numeroPartido, codigoMunicipioCandidatura,
+					numeroUrna, nomeUrna, situacaoCandidatura, nomeCargo, situacaoEleicao, 
+					determinarSeSegundoTurno(numeroTurno)));
 		}
 	}
 
@@ -120,16 +121,19 @@ public abstract class PovoadorGenerico {
 		return numeroTurno == 2;
 	}
 
-	private void criarCandidatoSeNaoExistir(Long cpf, int codigoMunicipioNascimento, int codigoNacionalidade,
+	private Candidato criarCandidatoSeNaoExistir(Long cpf, int codigoMunicipioNascimento, int codigoNacionalidade,
 			int codigoOcupacao, int codigoGrauInstrucao, String nomeCandidato, String dataNascimento,
 			BigInteger numeroTitulo, String email, String sexo, String estadoCivil) {
 
 		Candidato c = getEntityManager().find(Candidato.class, cpf);
 
 		if (c == null) {
-			getEntityManager().persist(new Candidato(cpf, codigoMunicipioNascimento, codigoNacionalidade, codigoOcupacao, 
-					codigoGrauInstrucao, nomeCandidato, converterData(dataNascimento), numeroTitulo, email, sexo, estadoCivil));
+			c = new Candidato(cpf, codigoMunicipioNascimento, codigoNacionalidade, codigoOcupacao, codigoGrauInstrucao,
+					nomeCandidato, converterData(dataNascimento), numeroTitulo, email, sexo, estadoCivil);
+			getEntityManager().persist(c);
 		}
+
+		return c;
 	}
 
 	private Date converterData(String data) {
@@ -161,44 +165,36 @@ public abstract class PovoadorGenerico {
 		}
 	}
 
-	private void criarPartidoSeNaoExistir(int numeroPartido, String siglaPartido, String nomePartido) {
+	private PartidoPolitico criarPartidoSeNaoExistir(int numeroPartido, String siglaPartido, String nomePartido) {
 		PartidoPolitico p = getEntityManager().find(PartidoPolitico.class, numeroPartido);
 		if (p == null) {
-			getEntityManager().persist(new PartidoPolitico(numeroPartido, siglaPartido, nomePartido));
+			p = new PartidoPolitico(numeroPartido, siglaPartido, nomePartido);
+			getEntityManager().persist(p);
 		}
+
+		return p;
 	}
 
 	@SuppressWarnings("unchecked")
-	private Municipio criarMunicipioSeNaoExistir(int codigoMunicipio, String nomeMunicipio, UF uf) {
+	private Municipio criarMunicipioSeNaoExistir(String nomeMunicipio, UF uf) {
 		Municipio m = null;
-		if(codigoMunicipio == -1) {
-			List<Municipio> ms = getEntityManager()
-					.createQuery("from Municipio m where m.nome = :nomeMunicipio and m.uf = :uf")
-					.setParameter("nomeMunicipio", nomeMunicipio)
-					.setParameter("uf", uf)
-					.getResultList();
-			
-			if(ms != null && ms.size() == 1) {
-				m = ms.get(0);
+
+		List<Municipio> ms = getEntityManager()
+				.createQuery("from Municipio m where m.nome = :nomeMunicipio and m.uf = :uf")
+				.setParameter("nomeMunicipio", nomeMunicipio).setParameter("uf", uf).getResultList();
+
+		if (ms != null && ms.size() == 1) {
+			m = ms.get(0);
+		} else {
+			Integer codigoMunicipio = (Integer) getEntityManager()
+					.createQuery("select max(m.codigoTSE) from Municipio m").getSingleResult();
+			if(codigoMunicipio == null) {
+				codigoMunicipio = 0;
 			}
-			else {
-				Integer max = (Integer) getEntityManager().createQuery("select max(m.id) from Municipio m").getSingleResult();
-				m = new Municipio(max + 100000, nomeMunicipio, uf);
-				getEntityManager().persist(m);
-			}
+			m = new Municipio(codigoMunicipio + 1, nomeMunicipio, uf);
+			getEntityManager().persist(m);
 		}
-		else {
-			m = getEntityManager().find(Municipio.class, codigoMunicipio);
-			if (m == null) {
-				m = new Municipio(codigoMunicipio, nomeMunicipio, uf);
-				getEntityManager().persist(m);
-			}
-		}
-		
-		if(m == null) {
-			throw new RuntimeException("Não foi possível recuperar o município");
-		}
-		
+
 		return m;
 	}
 
@@ -221,5 +217,4 @@ public abstract class PovoadorGenerico {
 		}
 		return entityManager;
 	}
-
 }
