b90defdc6436a53346dde36ca8cf2bc8b49962ab
Welder Luz <wweellddeerr@gmail.com>
2016-12-08 20:49:58 -0200

    Evoluções.

--- Scripts/criacao_view.sql
+++ Scripts/criacao_view.sql
@@ -15,5 +15,40 @@ on c.cpf = cr.cpf_candidato
 inner join municipio m
 on m.cod_tse = cr.cod_tse_munic_eleicao
 group by m.cod_tse, cr.ano_eleicao, cr.cargo, c.sexo
-having count(*) = 0
 order by 1, 2, 3, 4;
+
+
+select m.sigla_uf, count(*)
+from municipio m
+where m.cod_tse in (
+	select distinct cr.cod_tse_munic_eleicao
+	from candidatura cr
+	inner join candidato c
+	on cr.cpf_candidato = c.cpf
+	where not exists (
+		select *
+		from candidatura cr2
+		inner join candidato c2
+		on cr2.cpf_candidato = c2.cpf
+		where cr2.cpf_candidato = cr.cpf_candidato
+		and cr2.ano_eleicao = cr.ano_eleicao
+		and c2.cpf = c.cpf
+		and c2.sexo = 'FEMININO'
+	)
+)
+group by m.sigla_uf;
+
+select count(distinct  cr.cod_tse_munic_eleicao)
+	from candidatura cr
+	inner join candidato c
+	on cr.cpf_candidato = c.cpf
+	where not exists (
+		select *
+		from candidatura cr2
+		inner join candidato c2
+		on cr2.cpf_candidato = c2.cpf
+		where cr2.cpf_candidato = cr.cpf_candidato
+		and cr2.ano_eleicao = cr.ano_eleicao
+		and c2.cpf = c.cpf
+		and c2.sexo = 'FEMININO'
+	)
