44e3e4cae6050ee4e0f09a7a082535495045189a
Welder Luz <wweellddeerr@gmail.com>
2016-12-08 22:30:28 -0200

    FIM.

--- Scripts/criacao_view.sql
+++ Scripts/criacao_view.sql
@@ -1,54 +1,30 @@
-create view vw_agrupamento_cargo_sexo as
-select cr.ano_eleicao, cr.cargo, c.sexo, count(*)
-from candidatura cr
-inner join candidato c
-on c.cpf = cr.cpf_candidato
-group by cr.ano_eleicao, cr.cargo, c.sexo
-order by 1, 2, 3;
 
-
-
-select m.cod_tse, cr.ano_eleicao, cr.cargo, c.sexo, count(*)
-from candidatura cr
-inner join candidato c
-on c.cpf = cr.cpf_candidato
-inner join municipio m
-on m.cod_tse = cr.cod_tse_munic_eleicao
-group by m.cod_tse, cr.ano_eleicao, cr.cargo, c.sexo
-order by 1, 2, 3, 4;
-
-
-select m.sigla_uf, count(*)
-from municipio m
-where m.cod_tse in (
-	select distinct cr.cod_tse_munic_eleicao
+	create view agrupamento_partido_eleitos as
+	select CAST(ca.numero_partido AS CHAR(100)) AS PARTIDO, count(*)
+	from candidatura ca
+	inner join candidato c
+	on c.cpf = ca.cpf_candidato
+	where ca.SITUACAO_ELEICAO like 'ELEITO%'
+	group by ca.numero_partido
+	having count(*) > 1000
+	order by 2 desc, 1;	
+	
+	create view vw_agrupamento_cargo_sexo as
+	select cr.ano_eleicao, cr.cargo, c.sexo, count(*)
 	from candidatura cr
 	inner join candidato c
-	on cr.cpf_candidato = c.cpf
-	where not exists (
-		select *
-		from candidatura cr2
-		inner join candidato c2
-		on cr2.cpf_candidato = c2.cpf
-		where cr2.cpf_candidato = cr.cpf_candidato
-		and cr2.ano_eleicao = cr.ano_eleicao
-		and c2.cpf = c.cpf
-		and c2.sexo = 'FEMININO'
-	)
-)
-group by m.sigla_uf;
+	on c.cpf = cr.cpf_candidato
+	group by cr.ano_eleicao, cr.cargo, c.sexo
+	order by 1, 2, 3;
 
-select count(distinct  cr.cod_tse_munic_eleicao)
-	from candidatura cr
-	inner join candidato c
-	on cr.cpf_candidato = c.cpf
-	where not exists (
-		select *
-		from candidatura cr2
-		inner join candidato c2
-		on cr2.cpf_candidato = c2.cpf
-		where cr2.cpf_candidato = cr.cpf_candidato
-		and cr2.ano_eleicao = cr.ano_eleicao
-		and c2.cpf = c.cpf
-		and c2.sexo = 'FEMININO'
+	create view vw_ocupacao_que_nao_elege as
+	select o.descricao as nome_ocupacao
+	from ocupacao o
+	where o.cod not in (
+		select distinct c2.cod_ocupacao 
+		from candidato c2
+		inner join candidatura c3
+		on c2.cpf = c3.cpf_candidato
+		where c3.SITUACAO_ELEICAO like 'ELEITO%'
 	)
+	order by 1; 
